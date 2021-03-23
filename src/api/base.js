79ec62e241d5898649ffd8f4730ea12cd074eae5
buai.cr <2732914121@qq.com>
2021-03-23 09:14:50 +0800

    迭代

--- src/api/base.js
+++ src/api/base.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-export function Request(method = "get", url, data = null, timeout = 2000) {
+export function Request(
+    method = "get",
+    url,
+    data = null,
+    baseURL = null,
+    timeout = 2000
+) {
     return new Promise((resolve, reject) => {
         axios({
-            baseURL: "http://172.30.145.128:8800/", // 可修改基础域名
+            baseURL: !!baseURL ? baseURL : "http://172.30.145.128:8800/", // 可修改基础域名,当然，支持部分api自定义baseURL
             method: method,
             url,
             timeout,
---
+++ src/globalConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    baseUrl: "http://gp.yuntaigo.com/", // 这里可以填写全局的域名
+};
