5b4744c84ec91cdb8c1a4cefb215c909a97b5d2f
陈睿 <2732914121@qq.com>
2021-05-12 11:25:48 +0800

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Selector from "./pages/Selector";
 import Tutorial from "./pages/Tutorial";
 
 function App() {
-    return (
-        <div className="page">
-            <div className="hello">
-                <img src={Img} className="img" alt="" />
-                <div className="text">欢迎使用脚手架</div>
-                <div className="text2">请根据实际开发删除对应的文件和文件夹，本脚手架既是教手架也是示例项目</div>
-                <Switch>
-                    <Route path="/index" exact component={Selector} />
-                    <Route path="/routerTutorial" component={Tutorial}/>
-                    <Redirect exact to="/index" from="/" />
-                </Switch>
-            </div>
-        </div>
-    );
+	return (
+		<div className="page">
+			<div className="hello">
+				<img src={Img} className="img" alt="" />
+				<div className="text">欢迎使用脚手架</div>
+				<div className="text2">
+					请根据实际开发删除对应的文件和文件夹，本脚手架既是教手架也是示例项目
+				</div>
+				<Switch>
+					<Route path="/index" exact component={Selector} />
+					<Route path="/routerTutorial" component={Tutorial} />
+					<Redirect exact to="/index" from="/" />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
