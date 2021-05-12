ccf79201c21f06d81df82373fdfe7f6b9fb04ccb
陈睿 <2732914121@qq.com>
2021-05-12 14:06:32 +0800

    fixed

--- config/outputConfig.js
+++ config/outputConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
 	buildName: "", // 设置打包后的文件夹名称
 	indexHTMLName: "", // 设置打包之后的html名称,命名必须带 .html后缀
-	runLanHost: false, // 设置yarn start 是否为本机ip启动，为true则是localhost启动
+	runLanHost: false, // 设置yarn start 是否为本机ip启动，为false则是localhost启动
 };
