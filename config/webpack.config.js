68257c8fc25158d0488a5fda1c7f099d82c838b7
陈睿 <2732914121@qq.com>
2021-05-12 13:42:30 +0800

    迭代

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,7 +26,7 @@ const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
 const ForkTsCheckerWebpackPlugin = require("react-dev-utils/ForkTsCheckerWebpackPlugin");
 const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const { buildName } = require("./outputConfig");
+const { buildName, indexHTMLName } = require("./outputConfig");
 const postcssNormalize = require("postcss-normalize");
 const appPackageJson = require(paths.appPackageJson);
 
@@ -591,7 +591,7 @@ module.exports = function (webpackEnv) {
 			// Generates an `index.html` file with the <script> injected.
 			new HtmlWebpackPlugin(
 				Object.assign(
-					{},
+					{ filename: `${indexHTMLName || "index.html"}` },
 					{
 						inject: true,
 						template: paths.appHtml,
